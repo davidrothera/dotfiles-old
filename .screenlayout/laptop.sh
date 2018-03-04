15b6001176e56eea220fb1a05a60b2d36879c0fe
David Rothera <david.rothera@gmail.com>
2018-03-03 18:23:05 -0800

    new laptop

--- .screenlayout/laptop.sh
+++ .screenlayout/laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x952 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
